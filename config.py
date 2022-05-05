f00a200c486a265323a4502e878e95783a7717db
Сергей Машоха <sergeymashokha@MacBook-Pro-Sergej.local>
2022-05-05 12:43:56 +0300

    Fix_4

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ pwd = "18ae36f1ff2507584601d9b4cf84261fb3522b620876d9f4bf95dd4626170351"
 hostname = "ec2-176-34-211-0.eu-west-1.compute.amazonaws.com"
 db = "dgtrrfeujfv6"
 
-token = "19836e6ac396213b8ff3588e408938558ed3fc00d5a4e9d3a76fd7dcfcd35c155caadf842df8a5d7f54b8"
+token = "bfd7cbba142e41d0b114b788ae3ff87758edb44a1dc39324a4b33912d8167fa8d1fe2484fd82b75af23e6"
