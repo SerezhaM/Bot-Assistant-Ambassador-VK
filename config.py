9b766db05a40ec649cfb965d78ac1804ce2f38ab
sergeymashokha <diPcym-8vimty-1743Ded-MoroZ-kysxac>
2022-05-31 11:59:18 +0300

    Fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-username ="ldxnfxhmfitvek"
-pwd = "328c202c0aae2f9d92373abc40a10d7d6b15c0bc3c3dc83cf21a1b35c5c100bf"
-hostname = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com"
-db = "d36s3t1700pl8r"
+username =""
+pwd = ""
+hostname = ""
+db = ""
 
 
